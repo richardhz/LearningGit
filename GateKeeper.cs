ac5cb134334fbc511d66d3ef4394f0162be73d69
Richard Hernandez <richardhz@users.noreply.github.com>
2016-08-14 18:10:40 +0100

    Just some files to play with

---
+++ GateKeeper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncCoreStuff
+{
+    public class GateKeeper
+    {
+        public DateTime currentTime { get; private set; }
+        public CancellationToken Token { get; }
+
+        private CancellationTokenSource _cancellationTokenSource;
+        private UdpClient server = null;
+        private IPEndPoint remoteEndPoint = null;
+        public GateKeeper()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            Token = _cancellationTokenSource.Token;    
+        }
+
+        public Task Listner()
+        {
+            server = new UdpClient(11000);
+            remoteEndPoint = new IPEndPoint(IPAddress.Any, 11000);
+            Task task = Task.Factory.StartNew(
+                () => Listen(Token),TaskCreationOptions.LongRunning
+                );
+            return task;
+        }
+
+        public void Cancel()
+        {
+            _cancellationTokenSource.Cancel();
+        }
+
+        private async Task Listen(CancellationToken ct)
+        {
+            while (!ct.IsCancellationRequested)
+            {
+                try
+                {
+                    var buffer = await server.ReceiveAsync();
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                currentTime = DateTime.UtcNow;
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncCoreStuff
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var gk = new GateKeeper();
+            int count = 0;
+            gk.Listner();
+            while(true)
+            {
+               
+                count++;
+                if (count == 60)
+                {
+                    gk.Cancel();
+                }
+
+                Console.WriteLine(gk.currentTime.ToString()+"->"+count.ToString());
+
+                Thread.Sleep(1000);
+                Console.Clear();
+
+                if(count > 65)
+                {
+                    break;
+                }
+            }
+           
+        }
+    }
+}
